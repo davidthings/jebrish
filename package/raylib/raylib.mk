4fd868e0eaa768e18cf190c25dbbd21676a7f799
David Williams <david@davidthings.com>
2023-11-28 11:07:01 -0800

    some extra help for raylib

--- package/raylib/raylib.mk
+++ package/raylib/raylib.mk
@@ -4,12 +4,19 @@
 #
 ################################################################################
 
+#
+#  When using DRM - xf86drm.h and xf86drmMode.h point to the wrong drm directory
+#  they need `#include <drm/drm.h>`
+#
+
 RAYLIB_VERSION = 5.0
 RAYLIB_SOURCE = $(RAYLIB_VERSION).tar.gz
 RAYLIB_SITE = https://github.com/raysan5/raylib/archive/refs/tags
 RAYLIB_INSTALL_STAGING = YES
 RAYLIB_INSTALL_TARGET = YES
-RAYLIB_CONF_OPTS = -DUSE_WAYLAND=ON -DBUILD_EXAMPLES=ON -DGLFW_BUILD_X11=OFF -DMAKE_INSTALL_PREFIX=/usr -DOPENGL_VERSION=2.1 -DGLFW_BUILD_X11=true
+# RAYLIB_CONF_OPTS = -DPLATFORM=DRM -DBUILD_EXAMPLES=ON -DMAKE_INSTALL_PREFIX=/usr
+RAYLIB_CONF_OPTS = -DUSE_WAYLAND=OFF -DBUILD_EXAMPLES=ON -DMAKE_INSTALL_PREFIX=/usr \
+                   -DOPENGL_VERSION=2.1 -DGLFW_BUILD_X11=ON 
 RAYLIB_DEPENDENCIES = mesa3d libgl wayland libxkbcommon wayland-protocols
 
 define RAYLIB_INSTALL_TARGET_CMDS
